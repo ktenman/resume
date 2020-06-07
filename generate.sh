603c86ce722d13899f708b11a4c08f9799070f24
Konstantin Tenman <ktenman@users.noreply.github.com>
2020-06-07 19:09:05 +0100

    Add missing article

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
+git reset --hard
+git pull
 docker build -t latex .
 docker run --rm -i -v "$PWD":/data latex pdflatex konstantin_tenman_resume.tex
 cp konstantin_tenman_resume.pdf /var/www/html/cv.pdf
