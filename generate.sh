3b74f43af4fd85633cc9cf0e5488909a27a88434
Konstantin Tenman <ktenman@users.noreply.github.com>
2020-06-07 19:11:05 +0100

    Use resume.pdf

--- generate.sh
+++ generate.sh
@@ -3,3 +3,4 @@ git pull
 docker build -t latex .
 docker run --rm -i -v "$PWD":/data latex pdflatex konstantin_tenman_resume.tex
 cp konstantin_tenman_resume.pdf /var/www/html/cv.pdf
+cp konstantin_tenman_resume.pdf /var/www/html/resume.pdf
